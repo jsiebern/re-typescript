0e9ee94b57cc5a58022785ea6cb4776db256fa1c
Jonathan Siebern <jsiebern88@gmail.com>
2020-09-05 23:57:22 +0200

    esy update

--- _napkin/format_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-FORMAT="$DIR/format.sh"
-
-for file in $(find $1 -type f -name "*.res")
-do
-  $FORMAT $file
-done 
